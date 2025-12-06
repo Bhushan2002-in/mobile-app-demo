0fe4b1a50a8f2819ddaea11394ddf712e5552c7f
Bhushan Soni <bhushansoni@Bhushans-MacBook-Pro.local>
2025-12-07 04:03:40 +0530

    Fixes of whitescreen

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   // Set base for GitHub Pages (repo name)
   base: '/mobile-app-demo/',
+  build: {
+    outDir: 'docs', // deploy from /docs on the main branch
+  },
   plugins: [react()],
 });
